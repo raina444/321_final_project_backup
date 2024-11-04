42cc3b5f68c344f6b478ea7ba9afd2f12124bc3e
raina444 <93680943+raina444@users.noreply.github.com>
2024-11-04 22:08:13 +0400

    Add files via upload

---
+++ Models/PasswordBoxBindingBehavior.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp_random_testing.Models
+{
+
+
+    public class PasswordBoxBindingBehavior : Behavior<PasswordBox>
+    {
+
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.Register("Password", typeof(string), typeof(PasswordBoxBindingBehavior), new PropertyMetadata(string.Empty));
+
+        public string Password
+        {
+            get { return (string)GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PasswordChanged += PasswordBox_PasswordChanged;
+            AssociatedObject.Loaded += (s, e) => AssociatedObject.Password = Password; // Set initial password
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.PasswordChanged -= PasswordBox_PasswordChanged;
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Password = AssociatedObject.Password;
+        }
+    }
+}
